2b0ad75e7fac98ecefe80b4415c8cbd807b9ffde
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-09 13:03:17 +0530

    Added: Event listener to validate phone number in input form

--- js/MobileForms.js
+++ js/MobileForms.js
@@ -9,7 +9,7 @@ const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input',function(){
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if(nameRegex.test(text.value)){
+    if(nameRegex.test(text.value)){ 
         textError.textContent = "";
     }else{
         textError.textContent = "Name is incorrect";
